623da49a221209b6b4fb6e9c0f627cfd2ba1c4ce
espedroso <espgordo@gmail.com>
2024-02-05 13:04:41 -0300

    inportando a pasta assets

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,5 +3,30 @@ import "./Header.css";
 
 
 const Header = () => {
+    return {<nav className="header__navigation">
+    <div className="navigation">
+        <button className="arrow-left">
+            <img src="./src/assets/icons/small-left.png" alt="Seta Esquerda">
+        </button>
+        <button className="arrow-right">
+            <img src="./src/assets/icons/small-right.png" alt="Seta Direita">
+        </button>
+    
+        <div className="header__search">
+            <img src="./src/assets/icons/search.png" alt="">
+            <input id="search-input" type="text" maxlength="800" placeholder="O que você quer ouvir?">
+        </div>
+    </div>
+        <div className="header__login">
+            <button className="subscribe">Inscreva-se
 
+            </button>
+            <button className="login">Entrar
+
+            </button>
+        </div>
+</nav>
 }
+            
+     
+};
